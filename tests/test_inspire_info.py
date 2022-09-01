e1a640db4d178087f8de0c2d7a0e6db8d9caaeb8
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-09-01 11:48:44 +0200

    Updates tests

--- tests/test_inspire_info.py
+++ tests/test_inspire_info.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
 
 """Tests for `inspire_info` package."""
-
-import pytest
-
-
-import inspire_info.myutils as myutils
--- tests/test_weird_publication.py
+++ tests/test_weird_publication.py
@@ -1,4 +1,3 @@
-import json
 import inspire_info.myutils as myutils
 
 query = "https://inspirehep.net/api/literature/346986"
@@ -6,5 +5,3 @@ query = "https://inspirehep.net/api/literature/346986"
 data = myutils.read_from_inspire(query)
 
 print(data)
-
-
