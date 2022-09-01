344a06b9b52713e47bf53ee4d3476746e82f6c23
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-09-01 09:44:59 +0200

    Adds functionality to create latex document

---
+++ create_latex_doc.py
@@ -0,0 +1,37 @@
+from inspire_info import LatexCreator
+
+template = r"""\documentclass[11pt]{article}
+
+\title{Multiple Bibliographies with \texttt{multibib}}
+\author{Tim Wolf}
+\date{}
+
+\usepackage[resetlabels,labeled]{multibib}
+\newcites{Math}{Math Readings}
+\newcites{Phys}{Physics Readings}
+
+\begin{document}
+
+\maketitle
+
+__NOCITES__
+
+\bibliographystyle{unsrt}
+\bibliography{references}
+
+% \bibliographystyleMath{unsrt}
+% \bibliographyMath{refs-etc}
+
+% \bibliographystylePhys{unsrt}
+% \bibliographyPhys{refs-etc}
+
+\end{document}
+"""
+
+
+if __name__ == "__main__":
+    document_maker = LatexCreator(template=template,
+                                  outdir="publication_latex",
+                                  folder="publications_bibtex")
+    document_maker.make_bibliography()
+    document_maker.create_latex_doc(filename="publications.tex")
