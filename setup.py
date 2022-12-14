1257916e1d7762600eb2a489c7f3ed7c59bccf92
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-12-13 21:42:02 -0500

    Relaxes requirements to meet 3.7

--- setup.py
+++ setup.py
@@ -18,15 +18,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Tim Wolf",
     author_email='tim.wolf@mpi-hd.mpg.de',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     description="Package to retrieve more complex info from inspire than the web-interface allows",
